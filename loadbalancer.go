922a3685fe1cc58c15345e7efa9316341ba9fb20
Jay <jgodara@sarvika.com>
2020-05-02 02:31:42 +0530

    Made mux update existing routes

--- loadbalancer.go
+++ loadbalancer.go
@@ -3,6 +3,7 @@ package stargate
 type LoadBalancer interface {
 	NextServer() *DownstreamServer
 	Length() int
+	UpdateRoutes([]string, DirectorFunc) error
 }
 
 type LoadBalancerMaker func([]string, DirectorFunc) (LoadBalancer, error)
--- stargate.go
+++ stargate.go
@@ -6,7 +6,12 @@ import (
 )
 
 type Proxy struct {
-	mux *mux.Router
+	mux           *mux.Router
+	lister        ServiceLister
+	balancerMaker LoadBalancerMaker
+	ctx           *Context
+	middleware    []Middleware
+	balancers     map[string]LoadBalancer
 }
 
 type Middleware func(*Context, http.Handler) http.HandlerFunc
@@ -15,57 +20,71 @@ func (s Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	s.mux.ServeHTTP(w, r)
 }
 
+func (s Proxy) Reload() {
+
+	rtr := mux.NewRouter()
+	routes := s.lister.ListAll()
+	for route, svc := range routes {
+		lb := s.balancers[route]
+		if lb != nil {
+			if err := lb.UpdateRoutes(svc, defaultDirector(s.ctx)); err != nil {
+				Logger.Errorf("Route %s not updated : %v", route, err)
+				continue
+			}
+			Logger.Debugf("Route %s updated", route)
+
+			continue
+		}
+
+		lb, err := s.balancerMaker(svc, defaultDirector(s.ctx))
+		if err != nil {
+			Logger.Errorf("Cannot create a loadBalancer for route %s : %v", route, err)
+			continue
+		}
+		s.balancers[route] = lb
+
+		handler := createHandler(s.ctx, lb, s.middleware)
+		//rtr.HandleFunc(route, handler)
+
+		rtr.NewRoute().Path(route).Handler(handler)
+
+		Logger.Infof("New route added - %s", route)
+	}
+
+	s.mux = rtr
+}
+
 func NewProxy(ctx *Context, l ServiceLister, loadBalancerMaker LoadBalancerMaker, mwf ...Middleware) (Proxy, error) {
 	r := mux.NewRouter()
 
 	routes := l.ListAll()
+	bm := map[string]LoadBalancer{}
 	for route, svc := range routes {
 		lb, err := loadBalancerMaker(svc, defaultDirector(ctx))
 		if err != nil {
+			Logger.Errorf("Cannot create a loadBalancer for route %s : %v", route, err)
 			return Proxy{}, err
 		}
+		bm[route] = lb
 
-		handler := http.HandlerFunc(serve(lb))
-		for _, m := range mwf {
-			handler = m(ctx, handler)
-		}
-
+		handler := createHandler(ctx, lb, mwf)
 		r.HandleFunc(route, handler)
 	}
 
-	return Proxy{r}, nil
+	return Proxy{
+		mux:           r,
+		lister:        l,
+		ctx:           ctx,
+		middleware:    mwf,
+		balancerMaker: loadBalancerMaker,
+		balancers:     bm,
+	}, nil
 }
 
-func serve(lb LoadBalancer) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var server *DownstreamServer
-
-		if lb.Length() > 0 {
-			serverCount := 0
-			for sv := lb.NextServer(); serverCount < lb.Length(); sv = lb.NextServer() {
-				if sv.IsAlive() {
-					server = sv
-					break
-				}
-				Logger.Debugf("Backend %s is not alive. Skipped.", sv.BaseURL)
-				serverCount++
-			}
-		}
-
-		if server == nil {
-			Logger.Errorf("No alive server available for route %s", r.URL)
-
-			w.Header().Add("Content-Type", "text/html")
-			w.WriteHeader(http.StatusServiceUnavailable)
-
-			_, err := w.Write([]byte(`<h1>503 Service Unavailable</h1>"`))
-			if err != nil {
-				Logger.Errorf("Unable to write response to client: %v\n", err)
-			}
-			return
-		}
-
-		Logger.Debugf("Resolved backend %s", server.BaseURL)
-		server.Backend.ServeHTTP(w, r)
+func createHandler(ctx *Context, lb LoadBalancer, mwf []Middleware) http.HandlerFunc {
+	handler := http.HandlerFunc(serve(lb))
+	for _, m := range mwf {
+		handler = m(ctx, handler)
 	}
+	return handler
 }
