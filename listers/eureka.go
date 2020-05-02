505a934c432abb937af25676523cbe1edd0e76a5
Jay <jgodara@sarvika.com>
2020-05-02 17:57:41 +0530

    Added documentation

--- listers/eureka.go
+++ listers/eureka.go
@@ -18,6 +18,9 @@ func (e eurekaLister) List(s string) []string {
 	return e.routes[s]
 }
 
+// ListAll queries the underlying eureka server and creates a route table based on registered applications and their
+// running instances. ListAll's implementation returns all instance regardless of their health status. Healthchecks
+// are done by Stargate.
 func (e eurekaLister) ListAll() (map[string][]string, error) {
 	apps, err := e.conn.GetApps()
 	if err != nil {
@@ -30,6 +33,7 @@ func (e eurekaLister) ListAll() (map[string][]string, error) {
 		r := fmt.Sprintf("/%s", strings.ToLower(a.Name))
 		for _, instance := range a.Instances {
 
+			// Use HTTPs path if the instance supports SSL.
 			url := fmt.Sprintf("http://%s:%d", instance.IPAddr, instance.Port)
 			if instance.SecurePortEnabled {
 				url = fmt.Sprintf("https://%s:%d", instance.IPAddr, instance.SecurePort)
@@ -43,6 +47,9 @@ func (e eurekaLister) ListAll() (map[string][]string, error) {
 	return e.routes, nil
 }
 
+// Eureka is an implementation of `stargate.ServiceLister` that adds support to list services registered in the passed
+// eureka server. The application name of each registered service is used as a route key, e.g, if an application
+// "SOME-App" is registered with Eureka, it will be accessible at /some-app.
 func Eureka(address string) stargate.ServiceLister {
 	// Get rid of those annoying messages
 	log.SetLevel(log.ERROR, "fargo")
