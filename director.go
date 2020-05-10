161b76f8e52f80f356d438dbb194224661ba8cf0
Jay <jgodara@sarvika.com>
2020-05-10 22:05:27 +0530

    Forwarding query parameters from origin

--- director.go
+++ director.go
@@ -1,6 +1,7 @@
 package stargate
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
 	"strings"
@@ -13,6 +14,7 @@ func defaultDirector(ctx *Context, route string) DirectorFunc {
 	return func(origin *url.URL) func(*http.Request) {
 		return func(r *http.Request) {
 			r.URL.Path = strings.Replace(r.URL.Path, route, "/", 1)
+			r.URL.RawQuery = combineQueries(origin, r.URL)
 
 			r.Header.Add("X-Forwarded-For", r.Host)
 			r.Header.Add("X-Origin-Host", origin.Host)
@@ -25,4 +27,22 @@ func defaultDirector(ctx *Context, route string) DirectorFunc {
 			r.URL.Host = origin.Host
 		}
 	}
+}
+
+func combineQueries(src *url.URL, dst *url.URL) string {
+	q := ""
+
+	if src.RawQuery != "" {
+		q = src.RawQuery
+	}
+
+	if dst.RawQuery != "" {
+		if q == "" {
+			q = dst.RawQuery
+		} else {
+			q = fmt.Sprintf("%s&%s", q, dst.RawQuery)
+		}
+	}
+
+	return q
 }
