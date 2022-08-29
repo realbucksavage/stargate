3b38777bc510f0db8e69a1e40963db20137aeacc
Jay <me@bsavage.xyz>
2022-08-29 14:57:43 +0530

    added doc to router options

--- router_options.go
+++ router_options.go
@@ -1,13 +1,17 @@
 package stargate
 
+// RouterOption represents a closure type that can be used to customize the behavior of Router created using NewRouter.
 type RouterOption func(r *Router)
 
+// WithMiddleware takes a middleware chain to be executed before all requests. The order of middleware passed to it is
+// preserved.
 func WithMiddleware(mw ...MiddlewareFunc) RouterOption {
 	return func(r *Router) {
 		r.middlewareFuncs = mw
 	}
 }
 
+// WithLoadBalancer lets you set the LoadBalancerMaker of your choice.
 func WithLoadBalancer(lb LoadBalancerMaker) RouterOption {
 	return func(r *Router) {
 		r.loadBalancerMaker = lb
